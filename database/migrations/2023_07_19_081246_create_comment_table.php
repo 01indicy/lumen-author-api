87b2ac639bf9f9f359dda3464baa095041fce99b
indiciez <justinmajura@gmail.com>
2023-07-19 14:53:13 +0300

    comments

---
+++ database/migrations/2023_07_19_081246_create_comment_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id('comment_id');
+            $table->unsignedBigInteger('book_id');
+            $table->string('description');
+            $table->timestamps();
+
+            $table->foreign('book_id')->references('id')->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
